f3451d175f8672cea46169103f6bfc98fedaf60b
julie <juliewah8785@gmail.com>
2024-11-26 11:42:52 +0800

    初版

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h3>和牛涮 日式鍋物放題 台北羅斯福店的用戶評論</h3>
+    <CommentInput @submitComment="addComment" />
+    <PreviousReview :comments="comments" />
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+// 留言框
+import CommentInput from './components/CommentInput.vue'
+// 先前的評論
+import PreviousReview from './components/PreviousReview.vue'
+
+const comments = ref([])
+
+const addComment = (newComment) => {
+  comments.value.unshift(newComment)
+}
+</script>
+
